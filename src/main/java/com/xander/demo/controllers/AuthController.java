2e6db5a0f0304896f6cc901825edc815ed726951
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2022-11-08 12:13:54 +0300

    fix

--- src/main/java/com/xander/demo/controllers/AuthController.java
+++ src/main/java/com/xander/demo/controllers/AuthController.java
@@ -1,27 +1,28 @@
-package com.xander.demo.controllers;
+// package com.xander.demo.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.lang.NonNull;
+// import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.nimbusds.oauth2.sdk.AccessTokenResponse;
+// import com.nimbusds.oauth2.sdk.AccessTokenResponse;
 
-@RestController
-@RequestMapping("/api/auth")
-public class AuthController {
-  private final AuthService authService;
+// @RestController
+// @RequestMapping("/api/auth")
+// public class AuthController {
+// private final AuthService authService;
 
-  @PostMapping("/login")
-  @PreAuthorize("permitAll()")
-  public ResponseEntity<LoginResponseMessage> login(String email, String pass) {
-    var responseMessage = authService.login(loginRequestMessage);
+// @PostMapping("/login")
+// @PreAuthorize("permitAll()")
+// public ResponseEntity<LoginResponseMessage> login(String email, String pass)
+// {
+// var responseMessage = authService.login(loginRequestMessage);
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(responseMessage);
-  }
-}
+// return ResponseEntity.status(HttpStatus.OK)
+// .body(responseMessage);
+// }
+// }
